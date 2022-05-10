0a352f0225b471c90b08622faaef93182916df8c
adirangan <adirangan@yahoo.com>
2022-05-10 15:00:57 -0400

    updating dir_lakcluster_c_dev for jeremy elman

--- rsync_from_eval1_to_github_commit_push.sh
+++ rsync_from_eval1_to_github_commit_push.sh
@@ -27,3 +27,4 @@ cd /home/rangan/dir_bcc/dir_lakcluster_c_dev ;
 #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
 #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
 #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
+#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
