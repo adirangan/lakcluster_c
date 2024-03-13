5d40b0dcbf496290332b60794e960eeddae1dbae
adirangan <adirangan@yahoo.com>
2024-03-12 23:05:08 -0400

    updating dir_lakcluster_c_dev for jeremy elman

--- dir_m_dependencies/function_template.m
+++ dir_m_dependencies/function_template.m
@@ -10,10 +10,16 @@ function_template( ...
 
 str_thisfunction = 'function_template';
 
+if nargin<1;
+disp(sprintf(' %% testing %s',str_thisfunction));
+
+disp('returning'); return;
+end;%if nargin<1;
+
 na=0;
 if (nargin<1+na); parameter=[]; end; na=na+1;
 
-if isempty(parameter); parameter=struct('type','parameter); end;
+if isempty(parameter); parameter=struct('type','parameter'); end;
 if ~isfield(parameter,'flag_verbose'); parameter.flag_verbose=0; end;
 flag_verbose=parameter.flag_verbose;
 
